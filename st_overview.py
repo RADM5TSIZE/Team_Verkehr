feafdf1b04da707f988708c5e64cac8df321a9dd
RADM5TSIZE <mo.w.mueller@gmail.com>
2022-07-29 12:50:14 +0200

    update

--- st_overview.py
+++ st_overview.py
@@ -6,7 +6,29 @@ import pandas as pd
 
 
 def app():
-    st.write("""# Title""")
+    st.write("""# General Overview""")
+
+    with st.expander("See explanation"):
+        st.text("""
+        The Form below offers a dropdown menu to select a Intersection and get an overview for the selected Intersection. 
+        The radio controls offer a basic control over the shown data. 
+
+        Granularity: 
+            Single Lightsignals - shows every lightsignals from the Intersection 
+            Grouped Lightsignals - Groups the lightsignals to each direction
+            Full Intersection - aggregates all lightsignals
+        
+        Design:
+            Line Chart - shows intersection in one combined line chart
+            Bar Chart - creates bar chart for every intersection
+
+        Time Frame:
+            Yearly - aggregates data yearly
+            Monthly - aggregates data monthly without respecting year (e.g. January for every Year is aggregated)
+            Daily - aggregates data daily without respecting year and month
+            Hourly - aggregates data hourly without respecting year, month and day
+         
+     """)
 
     intersection = [".".join(f.split(".")[:-1])
                     for f in os.listdir(r".\data")]
@@ -25,7 +47,7 @@ def app():
             design = st.radio(label="Choose Design", options=["Bar Chart", "Line Chart"])
 
         with col3:
-            time_frame = st.radio(label="Choose Time Frame", options=["Yearly", "Monthly", "Hourly"])
+            time_frame = st.radio(label="Choose Time Frame", options=["Yearly", "Monthly", "Daily", "Hourly"])
 
         submitted = st.form_submit_button(label="Select")
 
@@ -37,7 +59,9 @@ def app():
         except pd.errors.ParserError as e:
             st.write("""### Not Supported Intersection""")
             return
-
+        except pd.errors.EmptyDataError as e:
+            st.write("""### Not Supported Intersection""")
+            return
 
         data = pd.read_csv(r".\data" +
                                "\\" + option + r".csv", delimiter=";")
@@ -45,47 +69,58 @@ def app():
         data['datetime'] = pd.to_datetime(
             data['datetime'], format='%d.%m.%Y %H:%M')
         
-        if granularity == "Grouped Lightsignals":
-            data = data.set_index('datetime')
+        try: 
+            if granularity == "Grouped Lightsignals":
+                data = data.set_index('datetime')
 
-            grouped_data = data.groupby(data.columns.str.extract(
-            "Bel_D(\d)\d", expand=False), axis=1).sum()
+                grouped_data = data.groupby(data.columns.str.extract(
+                "Bel_D(\d)\d", expand=False), axis=1).sum()
 
-            grouped_data.columns = ["Bel_D" + col + "x" for col in grouped_data.columns]
+                grouped_data.columns = ["Bel_D" + col + "x" for col in grouped_data.columns]
 
-            plot_data = grouped_data.reset_index()
+                plot_data = grouped_data.reset_index()
 
-            if time_frame == "Yearly":
-                plot_data = plot_data.groupby(plot_data['datetime'].dt.year).mean()
-            elif time_frame == "Monthly":
-                plot_data = plot_data.groupby(plot_data['datetime'].dt.month).mean()
-            elif time_frame == "Hourly":
-                plot_data = plot_data.groupby(plot_data['datetime'].dt.hour).mean()
+                if time_frame == "Yearly":
+                    plot_data = plot_data.groupby(plot_data['datetime'].dt.year).mean()
+                elif time_frame == "Monthly":
+                    plot_data = plot_data.groupby(plot_data['datetime'].dt.month).mean()
+                elif time_frame == "Daily":
+                    plot_data = plot_data.groupby(plot_data['datetime'].dt.day).mean()
+                elif time_frame == "Hourly":
+                    plot_data = plot_data.groupby(plot_data['datetime'].dt.hour).mean()
+                    
+            elif granularity == "Single Lightsignals":
+                if time_frame == "Yearly":
+                    plot_data = data.filter(like='Bel').groupby(data['datetime'].dt.year).mean()
+                elif time_frame == "Monthly":
+                    plot_data = data.filter(like='Bel').groupby(data['datetime'].dt.month).mean()
+                elif time_frame == "Daily":
+                    plot_data = data.filter(like='Bel').groupby(data['datetime'].dt.day).mean()
+                elif time_frame == "Hourly":
+                    plot_data = data.filter(like='Bel').groupby(data['datetime'].dt.hour).mean()
                 
-        elif granularity == "Single Lightsignals":
-            if time_frame == "Yearly":
-                plot_data = data.filter(like='Bel').groupby(data['datetime'].dt.year).mean()
-            elif time_frame == "Monthly":
-                plot_data = data.filter(like='Bel').groupby(data['datetime'].dt.month).mean()
-            elif time_frame == "Hourly":
-                plot_data = data.filter(like='Bel').groupby(data['datetime'].dt.hour).mean()
             
-        
-        elif granularity == "Full Intersection":
-            if time_frame == "Yearly":
-                plot_data = data.filter(like="Bel").sum(axis=1).groupby(data['datetime'].dt.year).mean()
-            elif time_frame == "Monthly":
-                plot_data = data.filter(like="Bel").sum(axis=1).groupby(data['datetime'].dt.month).mean()
-            elif time_frame == "Hourly":
-                plot_data = data.filter(like="Bel").sum(axis=1).groupby(data['datetime'].dt.hour).mean()
+            elif granularity == "Full Intersection":
+                if time_frame == "Yearly":
+                    plot_data = data.filter(like="Bel").sum(axis=1).groupby(data['datetime'].dt.year).mean()
+                elif time_frame == "Monthly":
+                    plot_data = data.filter(like="Bel").sum(axis=1).groupby(data['datetime'].dt.month).mean()
+                elif time_frame == "Monthly":
+                    plot_data = data.filter(like="Bel").sum(axis=1).groupby(data['datetime'].dt.day).mean()
+                elif time_frame == "Hourly":
+                    plot_data = data.filter(like="Bel").sum(axis=1).groupby(data['datetime'].dt.hour).mean()
 
-        if design == "Bar Chart":            
+            if design == "Bar Chart":            
+                
+                if(granularity == "Full Intersection"):
+                    st.bar_chart(plot_data)
+                else:
+                    for column in plot_data.columns:                
+                        st.bar_chart(plot_data[column])
             
-            if(granularity == "Full Intersection"):
-                st.bar_chart(plot_data)
-            else:
-                for column in plot_data.columns:                
-                    st.bar_chart(plot_data[column])
-        
-        elif design == "Line Chart":
-            st.line_chart(plot_data)
+            elif design == "Line Chart":
+                st.line_chart(plot_data)
+        except:
+            st.write("""### No Plots created""")
+            return
+
--- st_specific_overview.py
+++ st_specific_overview.py
@@ -11,55 +11,86 @@ def v_spacer(height, sb=False) -> None:
         else:
             st.write('\n')
 
+
 def app():
 
-    st.write("""# Title""")
+    st.write("""# Specific Overview""")
+    with st.expander("See explanation"):
+
+        st.text("""
+        The Form below offers a dropdown menu to select a Intersection and inputs to specify date and time. Also the specific lightsignal / part of intersection needs to be specified
+        The result is an overview of the traffic at the specified time
+
+        Granularity: 
+            Single - every lightsignal is selectable
+            Grouped - part of intersection is selectable
+            Full - groups full intersection and removes dropdown
+        
+        Date:
+            Date Picker: fixed min 01.06.2020 - max 01.06.2022
+
+        Intersection:
+            Choose Intersection from dropdown
+
+        Part of intersection:
+            updates automatically
+            specify part of intersection
+     """)
+
 
     intersection = [".".join(f.split(".")[:-1])
-                        for f in os.listdir(r".\data")]
+                    for f in os.listdir(r".\data")]
 
     option = st.selectbox(
-            "Choose intersection", intersection, key="option")  # , on_change=inters_select, key="inters"
+        "Choose intersection", intersection, key="option")  # , on_change=inters_select, key="inters"
 
     col1, col2 = st.columns(2)
 
     with col1:
         date_input = st.date_input("Enter Date:", value=date(
-                2020, 6, 1), min_value=date(2020, 6, 1), max_value=date(2022, 6, 1))
+            2020, 6, 1), min_value=date(2020, 6, 1), max_value=date(2022, 6, 1))
 
     with col2:
         time_input = st.selectbox("Enter Time:", options=["00:00",
-                                                            "01:00",
-                                                            "02:00",
-                                                            "03:00",
-                                                            "04:00",
-                                                            "05:00",
-                                                            "06:00",
-                                                            "07:00",
-                                                            "08:00",
-                                                            "09:00",
-                                                            "10:00",
-                                                            "11:00",
-                                                            "12:00",
-                                                            "13:00",
-                                                            "14:00",
-                                                            "15:00",
-                                                            "16:00",
-                                                            "17:00",
-                                                            "18:00",
-                                                            "19:00",
-                                                            "20:00",
-                                                            "21:00",
-                                                            "22:00",
-                                                            "23:00"
-                                                            ])
+                                                          "01:00",
+                                                          "02:00",
+                                                          "03:00",
+                                                          "04:00",
+                                                          "05:00",
+                                                          "06:00",
+                                                          "07:00",
+                                                          "08:00",
+                                                          "09:00",
+                                                          "10:00",
+                                                          "11:00",
+                                                          "12:00",
+                                                          "13:00",
+                                                          "14:00",
+                                                          "15:00",
+                                                          "16:00",
+                                                          "17:00",
+                                                          "18:00",
+                                                          "19:00",
+                                                          "20:00",
+                                                          "21:00",
+                                                          "22:00",
+                                                          "23:00"
+                                                          ])
     col3, col4 = st.columns(2)
     with col3:
         granularity = st.radio(label="Choose Granularity", options=[
-                                "Single", "Grouped"])
+            "Single", "Grouped", "Full"])
 
-    data = pd.read_csv(r".\data" +
-                        "\\" + option + r".csv", delimiter=";")
+    try:
+        data = pd.read_csv(r".\data" +
+                           "\\" + option + r".csv", delimiter=";")
+    except pd.errors.ParserError as e:
+        st.write("""### Not Supported Intersection""")
+        return
+    except pd.errors.EmptyDataError as e:
+        st.write("""### Not Supported Intersection""")
+        return
+
     data.rename(columns={'01.06.2020 00:00': 'datetime'}, inplace=True)
     data['datetime'] = pd.to_datetime(
         data['datetime'], format='%d.%m.%Y %H:%M')
@@ -71,9 +102,9 @@ def app():
         elif granularity == "Grouped":
             grouped_data = data.set_index('datetime')
             grouped_data = data.groupby(data.columns.str.extract(
-                    "Bel_D(\d)\d", expand=False), axis=1).sum()
+                "Bel_D(\d)\d", expand=False), axis=1).sum()
             grouped_data.columns = ["Bel_D" + col +
-                                        "x" for col in grouped_data.columns]
+                                    "x" for col in grouped_data.columns]
             grouped_data = grouped_data.reset_index()
             specific_intersection = st.selectbox(
                 label="Choose the part of Intersection", options=grouped_data.filter(like='Bel').columns)
@@ -81,31 +112,162 @@ def app():
     if st.button("Select"):
         st.write("""# Results""")
         if granularity == "Single":
-            time_value = datetime.strptime(str(date_input) + time_input, r"%Y-%m-%d%H:%M")
-            value = data[data["datetime"] == time_value][specific_intersection].values[0]
-            st.write(f"The traffic volume for the intersection on {date_input} at {time_input} is {value}")
+            time_value = datetime.strptime(
+                str(date_input) + time_input, r"%Y-%m-%d%H:%M")
+            value = data[data["datetime"] ==
+                         time_value][specific_intersection].values[0]
+
+
+            col_Rgeneral1, col_Rgeneral2, col_Rgeneral3, filler = st.columns(4)
+            col_Rgeneral1.write("""### Date""")
+            col_Rgeneral1.metric(label="Year-Month-Day ", value=str(date_input))
+
+            col_Rgeneral2.write("""### Time""")
+            col_Rgeneral2.metric(label="Hour:Minute", value=str(time_input))
+
+            col_Rgeneral3.write("""### Traffic""")
+            col_Rgeneral3.metric(label="Amount of traffic", value=value)
+
+            st.markdown("""---""")
+
+            col_result1, col_result2, col_result3, col_result4 = st.columns(4)
+
+            if not time_value <= datetime.strptime("2020-06-01_00:00", r"%Y-%m-%d_%H:%M"):
+                last_value = data[data["datetime"] ==
+                                          time_value - timedelta(hours=1)][specific_intersection].values[0]
+                increase_hour = (value - last_value)/last_value * 100
+                col_result1.write("""### Change to last hour: """)
+                col_result1.metric(label="Last Hour", value=last_value, delta=f"{round(increase_hour, 1)}%")
+            
+            if not time_value <= datetime.strptime("2020-06-02_00:00", r"%Y-%m-%d_%H:%M"):
+                last_day_value = data[data["datetime"] ==
+                                               time_value - timedelta(days=1)][specific_intersection].values[0]
+                increase_day = (value - last_day_value)/last_day_value * 100
+                col_result2.write("""### Change to last day: """)
+                col_result2.metric(label="Last Day", value=last_day_value, delta=f"{round(increase_day, 1)}%")
+
+            if not time_value <= datetime.strptime("2020-06-08_00:00", r"%Y-%m-%d_%H:%M"):
+                last_week_value = data[data["datetime"] ==
+                                               time_value - timedelta(weeks=1)][specific_intersection].values[0]
+                increase_week = (value - last_week_value)/last_week_value * 100
+                col_result3.write("""### Change to last week: """)
+                col_result3.metric(label="Last Week", value=last_week_value, delta=f"{round(increase_week, 1)}%")
+
+            if not time_value <= datetime.strptime("2020-07-01_00:00", r"%Y-%m-%d_%H:%M"):
+                last_30_value = data[data["datetime"] ==
+                                               time_value - timedelta(days=30)][specific_intersection].values[0]
+                increase_30 = (value - last_30_value)/last_30_value * 100
+                col_result4.write("""### 30 day change: """)
+                col_result4.metric(label="30 days", value=last_30_value, delta=f"{round(increase_30, 1)}%")
+
         elif granularity == "Grouped":
             data = data.set_index('datetime')
             grouped_data = data
             grouped_data = data.groupby(data.columns.str.extract(
-                    "Bel_D(\d)\d", expand=False), axis=1).sum()
+                "Bel_D(\d)\d", expand=False), axis=1).sum()
             grouped_data.columns = ["Bel_D" + col +
-                                        "x" for col in grouped_data.columns]
-            grouped_data = grouped_data.reset_index().rename(columns={'index': 'datetime'})
+                                    "x" for col in grouped_data.columns]
+            grouped_data = grouped_data.reset_index().rename(
+                columns={'index': 'datetime'})
 
+            time_value = datetime.strptime(
+                str(date_input) + time_input, r"%Y-%m-%d%H:%M")
+            value = grouped_data[grouped_data["datetime"] ==
+                                 time_value][specific_intersection].values[0]            
+            
+            col_Rgeneral1, col_Rgeneral2, col_Rgeneral3, filler = st.columns(4)
+            col_Rgeneral1.write("""### Date""")
+            col_Rgeneral1.metric(label="Year-Month-Day ", value=str(date_input))
 
-            time_value = datetime.strptime(str(date_input) + time_input, r"%Y-%m-%d%H:%M")
-            value = grouped_data[grouped_data["datetime"] == time_value][specific_intersection].values[0]
-            st.write(f"The traffic volume for the intersection on {date_input} at {time_input} is {value}")
+            col_Rgeneral2.write("""### Time""")
+            col_Rgeneral2.metric(label="Hour:Minute", value=str(time_input))
+
+            col_Rgeneral3.write("""### Traffic""")
+            col_Rgeneral3.metric(label="Amount of traffic", value=value)
+
+            st.markdown("""---""")
+
+            col_result1, col_result2, col_result3, col_result4 = st.columns(4)
 
             if not time_value <= datetime.strptime("2020-06-01_00:00", r"%Y-%m-%d_%H:%M"):
-                last_value = grouped_data[grouped_data["datetime"] == time_value - timedelta(hours=1)][specific_intersection].values[0]
-                increase_day = (last_value - value)/value * 100
-                st.write(f"This is a change of {round(increase_day, 0)}% to the last hour.")
+                last_value = grouped_data[grouped_data["datetime"] ==
+                                          time_value - timedelta(hours=1)][specific_intersection].values[0]
+                increase_hour = (value - last_value)/last_value * 100
+                col_result1.write("""### Change to last hour: """)
+                col_result1.metric(label="Last Hour", value=last_value, delta=f"{round(increase_hour, 1)}%")
+            
+            if not time_value <= datetime.strptime("2020-06-02_00:00", r"%Y-%m-%d_%H:%M"):
+                last_day_value = grouped_data[grouped_data["datetime"] ==
+                                               time_value - timedelta(days=1)][specific_intersection].values[0]
+                increase_day = (value - last_day_value)/last_day_value * 100
+                col_result2.write("""### Change to last day: """)
+                col_result2.metric(label="Last Day", value=last_day_value, delta=f"{round(increase_day, 1)}%")
 
             if not time_value <= datetime.strptime("2020-06-08_00:00", r"%Y-%m-%d_%H:%M"):
-                last_week_value = grouped_data[grouped_data["datetime"] == time_value - timedelta(weeks=1)][specific_intersection].values[0]
-                increase_week = (last_week_value - value)/value * 100                
-                st.write(f"And a change {round(increase_week, 0)} to last week.")
+                last_week_value = grouped_data[grouped_data["datetime"] ==
+                                               time_value - timedelta(weeks=1)][specific_intersection].values[0]
+                increase_week = (value - last_week_value)/last_week_value * 100
+                col_result3.write("""### Change to last week: """)
+                col_result3.metric(label="Last Week", value=last_week_value, delta=f"{round(increase_week, 1)}%")
 
+            if not time_value <= datetime.strptime("2020-07-01_00:00", r"%Y-%m-%d_%H:%M"):
+                last_30_value = grouped_data[grouped_data["datetime"] ==
+                                               time_value - timedelta(days=30)][specific_intersection].values[0]
+                increase_30 = (value - last_30_value)/last_30_value * 100
+                col_result4.write("""### 30 day change: """)
+                col_result4.metric(label="30 days", value=last_30_value, delta=f"{round(increase_30, 1)}%")
+        
 
+        elif granularity == "Full":
+            data = data.set_index('datetime')
+            full_data = data
+            full_data = data.filter(like="Bel").sum(axis=1)
+            full_data = full_data.reset_index().rename(
+                columns={'index': 'datetime'})
+
+            time_value = datetime.strptime(
+                str(date_input) + time_input, r"%Y-%m-%d%H:%M")
+            value = full_data[full_data["datetime"] == time_value][0].values[0]
+            
+            col_Rgeneral1, col_Rgeneral2, col_Rgeneral3, filler = st.columns(4)
+            col_Rgeneral1.write("""### Date""")
+            col_Rgeneral1.metric(label="Year-Month-Day ", value=str(date_input))
+
+            col_Rgeneral2.write("""### Time""")
+            col_Rgeneral2.metric(label="Hour:Minute", value=str(time_input))
+
+            col_Rgeneral3.write("""### Traffic""")
+            col_Rgeneral3.metric(label="Amount of traffic", value=value)
+
+            st.markdown("""---""")
+
+            col_result1, col_result2, col_result3, col_result4 = st.columns(4)
+
+            if not time_value <= datetime.strptime("2020-06-01_00:00", r"%Y-%m-%d_%H:%M"):
+                last_value = full_data[full_data["datetime"] ==
+                                          time_value - timedelta(hours=1)][0].values[0]
+                increase_hour = (value - last_value)/last_value * 100
+                col_result1.write("""### Change to last hour: """)
+                col_result1.metric(label="Last Hour", value=last_value, delta=f"{round(increase_hour, 1)}%")
+            
+            if not time_value <= datetime.strptime("2020-06-02_00:00", r"%Y-%m-%d_%H:%M"):
+                last_day_value = full_data[full_data["datetime"] ==
+                                               time_value - timedelta(days=1)][0].values[0]
+                increase_day = (value - last_day_value)/last_day_value * 100
+                col_result2.write("""### Change to last day: """)
+                col_result2.metric(label="Last Day", value=last_day_value, delta=f"{round(increase_day, 1)}%")
+
+            if not time_value <= datetime.strptime("2020-06-08_00:00", r"%Y-%m-%d_%H:%M"):
+                last_week_value = full_data[full_data["datetime"] ==
+                                               time_value - timedelta(weeks=1)][0].values[0]
+                increase_week = (value - last_week_value)/last_week_value * 100
+                col_result3.write("""### Change to last week: """)
+                col_result3.metric(label="Last Week", value=last_week_value, delta=f"{round(increase_week, 1)}%")
+
+            if not time_value <= datetime.strptime("2020-07-01_00:00", r"%Y-%m-%d_%H:%M"):
+                last_30_value = full_data[full_data["datetime"] ==
+                                               time_value - timedelta(days=30)][0].values[0]
+                increase_30 = (value - last_30_value)/last_30_value * 100
+                col_result4.write("""### 30 day change: """)
+                col_result4.metric(label="30 days", value=last_30_value, delta=f"{round(increase_30, 1)}%")
+            
